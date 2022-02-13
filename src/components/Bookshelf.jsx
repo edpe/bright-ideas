50bbd264a2e4202ccdb0c2aadc48754e93d1b4d2
edpe <ed@edperkins.co.uk>
2022-02-13 23:22:18 +0000

    WIP: New bookshelf design

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Bookshelf.module.scss";
 
 export const Book = ({ link, book }) => {
   return (
     <Link href={link} as={book.slug} passHref>
       <div className={styles.book}>
+        <div className={styles.bookImage}>
+          <Image src={"/images/leaves1.jpeg"} layout="fill" alt="book image" />
+        </div>
         <h3 className={styles.bookTitle}>{book.title}</h3>
       </div>
     </Link>
