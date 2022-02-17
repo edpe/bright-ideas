9777774679b0b54c55e86eb2ae92de6c2035c459
edpe <ed@edperkins.co.uk>
2022-02-17 19:52:37 +0000

    feat: home screen mobile layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import styles from "./Header.module.scss";
 
 const Header = () => (
   <div className={styles.header}>
-    <h1 className={styles.heading}>Bright Ideas</h1>
+    <Link href="/" passHref>
+      <h1 className={styles.heading}>Bright Ideas</h1>
+    </Link>
     <nav>
       <div className={styles.navWrapper}>
         <Link href="/" passHref>
