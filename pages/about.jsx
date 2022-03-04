330b7244d7ecb7c89e5eaf88bcce40d107051f3c
edpe <ed@edperkins.co.uk>
2022-03-04 21:12:17 +0000

    feat: update about text

--- pages/about.jsx
+++ pages/about.jsx
@@ -20,11 +20,20 @@ const about = () => (
             </p>
             <p>
               This site has been designed and written by Ed Perkins with help
-              digitizing the poems from Sara Perkins.
+              digitizing the poems from Sara Perkins. As an arist, Pat had a
+              love of vibrancy and color and the site has been designed to
+              reflect this, featuring some of her favourites.
             </p>
+
             <p>
               {
                 "We hope that whoever reads these poems enjoys them and finds the ideas in them helpful, as that's what Pat would have wished for them."
+              }
+            </p>
+
+            <p style={{ fontStyle: "italic", marginTop: "4rem" }}>
+              {
+                "All images used on the site are taken from Pat's garden where she worked hard and found much happiness."
               }
             </p>
           </div>
