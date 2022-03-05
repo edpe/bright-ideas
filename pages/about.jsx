7cee780be7ea8e333f7a059c8dd1655491eb659d
edpe <ed@edperkins.co.uk>
2022-03-05 11:31:03 +0000

    WIP: biog photo

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../src/components/Layout";
 import Header from "../src/components/Header";
+import Image from "next/image";
 
 import styles from "../styles/global.module.scss";
 
@@ -11,31 +12,42 @@ const about = () => (
       <div className={styles.fullPageHeightCentre}>
         <div className={styles.halfPage}>
           <h2 className={styles.heading}>About</h2>
-          <div className={styles.text}>
-            <p>
-              Bright Ideas is an archive for the poems of Pat Perkins. It
-              features previous poetry collections Bright Ideas (2003), More
-              Bright Ideas (2008) and a new, previously unpublished collection,
-              Bright Ideas in Action written in 2012.
-            </p>
-            <p>
-              This site has been designed and written by Ed Perkins with help
-              digitizing the poems from Sara Perkins. As an artist, Pat had a
-              love of vibrancy and colour and the site has been designed to
-              reflect this, featuring some of her favourites.
-            </p>
+          <div className={styles.content}>
+            <div className={styles.text}>
+              <p>
+                Bright Ideas is an archive for the poems of Pat Perkins. It
+                features previous poetry collections Bright Ideas (2003), More
+                Bright Ideas (2008) and a new, previously unpublished
+                collection, Bright Ideas in Action written in 2012.
+              </p>
+              <p>
+                This site has been designed and written by Ed Perkins with help
+                digitizing the poems from Sara Perkins. As an artist, Pat had a
+                love of vibrancy and colour and the site has been designed to
+                reflect this, featuring some of her favourites.
+              </p>
 
-            <p>
-              {
-                "We hope that whoever reads these poems enjoys them and finds the ideas in them helpful, as that's what Pat would have wished for them."
-              }
-            </p>
+              <p>
+                {
+                  "We hope that whoever reads these poems enjoys them and finds the ideas in them helpful, as that's what Pat would have wished for them."
+                }
+              </p>
 
-            <p style={{ fontStyle: "italic", marginTop: "4rem" }}>
-              {
-                "All images used on the site are taken from Pat's garden where she worked hard and found much happiness."
-              }
-            </p>
+              <p style={{ fontStyle: "italic", marginTop: "4rem" }}>
+                {
+                  "All images used on the site are taken from Pat's garden where she worked hard and found much happiness."
+                }
+              </p>
+            </div>
+            <div className={styles.photo}>
+              <Image
+                src={`/images/pat_ed_photo.JPG`}
+                layout="fixed"
+                height={500}
+                width={500}
+                alt="Pat and Ed Perkins"
+              />
+            </div>
           </div>
         </div>
       </div>
