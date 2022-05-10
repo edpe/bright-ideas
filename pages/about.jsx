dc85259182439f743ac73498d6e65581d6e174c8
Ed Perkins <ed@edperkins.co.uk>
2022-05-10 19:09:24 +0100

    feat: update about page text

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,28 +13,33 @@ const about = () => (
           <h2 className={styles.heading}>About</h2>
           <div className={styles.text}>
             <p>
-              Bright Ideas is an archive for the poems of Pat Perkins. It
-              features previous poetry collections Bright Ideas (2003), More
-              Bright Ideas (2008) and a new, previously unpublished collection,
-              Bright Ideas in Action written in 2012.
+              The inspiration for these verses often came through being still,
+              listening, watching nature and working in her garden. Pat found
+              ideas which brought peace and comfort in her own family life which
+              she shared to help others.
+            </p>
+            <p>
+              As an artist, Pat had a love of vibrancy and colour and the site
+              has been designed to reflect this, featuring some of her
+              favourites.
+            </p>
+            <p>
+              Bright Ideas features previous poetry collections Bright Ideas
+              (2003), More Bright Ideas (2008) and a new, previously unpublished
+              collection, Bright Ideas in Action written in 2012.
+            </p>
+            <p>
+              We hope that whoever reads these poems enjoys them and finds the
+              ideas in them helpful, as that’s what Pat would have wished for
+              them.
             </p>
             <p>
               This site has been designed and written by Ed Perkins with help
-              digitizing the poems from Sara Perkins. As an artist, Pat had a
-              love of vibrancy and colour and the site has been designed to
-              reflect this, featuring some of her favourites.
+              digitizing the poems from Sara Perkins.
             </p>
-
             <p>
-              {
-                "We hope that whoever reads these poems enjoys them and finds the ideas in them helpful, as that's what Pat would have wished for them."
-              }
-            </p>
-
-            <p style={{ fontStyle: "italic", marginTop: "4rem" }}>
-              {
-                "All images used on the site are taken from Pat's garden where she worked hard and found much happiness."
-              }
+              All images used on the site are taken from Pat’s garden where she
+              worked hard but also pottered, pondered and found much happiness.
             </p>
           </div>
         </div>
